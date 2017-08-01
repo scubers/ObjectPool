bd4d424909fed1db02601a13aba6b460532b4e8c
王俊仁 <jr-wong@qq.com>
2017-08-01 16:35:15 +0800

    add umbrella.h

---
+++ Classes/ObjectPool.h
@@ -0,0 +1,15 @@
+//
+//  ObjectPool.h
+//  ObjectPool
+//
+//  Created by 王俊仁 on 2017/8/1.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#ifndef ObjectPool_h
+#define ObjectPool_h
+
+#import <ObjectPool/PoolManagedObjectWrapper.h>
+#import <ObjectPool/AbstractObjectPool.h>
+
+#endif /* ObjectPool_h */
